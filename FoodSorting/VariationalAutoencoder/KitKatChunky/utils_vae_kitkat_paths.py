45863e169967834b0be168d1b89333fe806ab741
Alina <53077706+alinasode@users.noreply.github.com>
2021-05-20 16:25:51 +0200

    Add files via upload

---
+++ FoodSorting/VariationalAutoencoder/KitKatChunky/utils_vae_kitkat_paths.py
@@ -0,0 +1,36 @@
+import os, sys, inspect
+
+#################################
+#    Define parent directory    #
+#################################
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+vaedir = os.path.dirname(currentdir)
+sys.path.insert(0, vaedir)
+
+parentdir = os.path.dirname(vaedir)
+sys.path.insert(0, parentdir)
+
+#evaldir = f'{currentdir}/evaluations'
+#sys.path.insert(0, evaldir)
+
+##################################################
+# Define work direction for generated datasets   #
+##################################################
+
+PATH = f'{parentdir}/generated_dataset/KitKatChunky/'
+
+#NORMAL_AUG   = f'{PATH}/NormalAugmentations'
+#ANOMALY_AUG  = f'{PATH}/AnomalyAugmentations'
+
+DATA_AUGMENTATION = f'{PATH}/DataAugmentation'
+NORMAL_TRAIN_AUG  = f'{DATA_AUGMENTATION}/NormalTrainAugmentations'
+NORMAL_VAL_AUG    = f'{DATA_AUGMENTATION}/NormalValidationAugmentations'
+NORMAL_TEST_AUG   = f'{DATA_AUGMENTATION}/NormalTestAugmentations'
+ANOMALY_AUG       = f'{DATA_AUGMENTATION}/AnomalyAugmentations'
+
+
+#################################
+#    Define image dimensions    #
+#################################
+hh, ww = 128, 128
