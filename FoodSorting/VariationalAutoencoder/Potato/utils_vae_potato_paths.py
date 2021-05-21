451898ee20e215a9aa939dac9401ee180dd0afcf
Alina <53077706+alinasode@users.noreply.github.com>
2021-05-21 11:01:43 +0200

    Update utils_vae_potato_paths.py

--- FoodSorting/VariationalAutoencoder/Potato/utils_vae_potato_paths.py
+++ FoodSorting/VariationalAutoencoder/Potato/utils_vae_potato_paths.py
@@ -11,18 +11,11 @@ sys.path.insert(0, vaedir)
 parentdir = os.path.dirname(vaedir)
 sys.path.insert(0, parentdir)
 
-#evaldir = f'{currentdir}/evaluations'
-#sys.path.insert(0, evaldir)
-
 ##################################################
 # Define work direction for generated datasets   #
 ##################################################
 
 PATH = f'{parentdir}/generated_dataset/Potato/'
-
-#PERFECT_AUG  = f'{PATH}/NormalAugmentations'
-#METAL_AUG    = f'{PATH}/Anomaly1Augmentations'
-#HOLLOW_AUG   = f'{PATH}/Anomaly2Augmentations'
 
 DATA_AUGMENTATION = f'{PATH}/DataAugmentation'
 NORMAL_TRAIN_AUG  = f'{DATA_AUGMENTATION}/NormalTrainAugmentations'
