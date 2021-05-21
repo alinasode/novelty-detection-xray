5cc3bbe7737f3728ed941405c75f2ea290344eed
Alina <53077706+alinasode@users.noreply.github.com>
2021-05-21 10:57:20 +0200

    Update utils_ae_kitkat_paths.py

--- FoodSorting/Autoencoder/KitKatChunky/utils_ae_kitkat_paths.py
+++ FoodSorting/Autoencoder/KitKatChunky/utils_ae_kitkat_paths.py
@@ -1,5 +1,4 @@
-import os, sys, inspect #, shutil
-#from os.path import join
+import os, sys, inspect
 
 #################################
 #    Define parent directory    #
@@ -17,9 +16,6 @@ sys.path.insert(0, parentdir)
 ##################################################
 
 PATH = f'{parentdir}/generated_dataset/KitKatChunky/'
-
-#NORMAL_AUG   = f'{PATH}/NormalAugmentations'
-#ANOMALY_AUG  = f'{PATH}/AnomalyAugmentations'
 
 DATA_AUGMENTATION = f'{PATH}/DataAugmentation'
 NORMAL_TRAIN_AUG  = f'{DATA_AUGMENTATION}/NormalTrainAugmentations'
